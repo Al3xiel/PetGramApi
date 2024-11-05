9d56106a1145cac5e8f9e4db14915e6e99213d2f
Al3xiel <gabriel280104casas@gmail.com>
2024-11-04 23:59:13 -0500

    feat(share): add auditable shared entity.

---
+++ src/main/java/petgram/petgramapi/shared/domain/model/entities/AuditableModel.java
@@ -0,0 +1,21 @@
+package petgram.petgramapi.shared.domain.model.entities;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import java.util.Date;
+
+public class AuditableModel {
+
+    @Getter
+    @CreatedDate
+    @Field("created_at")
+    private Date createdAt;
+
+    @Getter
+    @LastModifiedDate
+    @Field("updated_at")
+    private Date updatedAt;
+}
