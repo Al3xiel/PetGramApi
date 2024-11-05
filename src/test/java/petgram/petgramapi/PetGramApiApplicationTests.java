1346b4bf7e81da1f63cbfa8214b34709a7253167
Al3xiel <gabriel280104casas@gmail.com>
2024-11-04 23:50:22 -0500

    chore: setup project.

---
+++ src/test/java/petgram/petgramapi/PetGramApiApplicationTests.java
@@ -0,0 +1,13 @@
+package petgram.petgramapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetGramApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
